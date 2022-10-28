bbac37c8c7987a09692c63575d7aed7e448be7fa
maksat123456 <41636627+maksat123456@users.noreply.github.com>
2022-10-28 18:32:28 +0600

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -7,8 +7,9 @@ shareBtn.addEventListener('click', event => {
   if (navigator.share) {
     // Browser supports native share api
     navigator.share({
-      text: 'Please read this great article: ',
-      url: 'https://www.google.com/'
+      text: 'Фото салон Коппьютерный услуги Полиграфия || Максат: ',
+      url: 'https://maksat123456.github.io/link/',
+        img:'https://maksat123456.github.io/link/assets/img/%D0%A0%D0%B5%D0%B7%D0%B5%D1%80%D0%B2%D0%BD%D0%B0%D1%8F_%D0%BA%D0%BE%D0%BF%D0%B8%D1%8F_%D0%9A%D0%BE%D0%BD%D0%B2%D0%B5%D1%80%D1%82%202019.png'
     }).then(() => {
       console.log('Thanks for sharing!');
     })
